9cc1b50f954e1e4c84bdfc1ae4548f643aa495a0
thegirlwiththesquirrel <d.n.savvidi@student.tudelft.nl>
2025-05-23 18:58:10 +0800

    shd reproduction results

--- experiments/SHD/shd_train_rf.py
+++ experiments/SHD/shd_train_rf.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader, random_split
 from datetime import datetime
 import math
 import random
+
 import sys
 sys.path.append("../..")
 import snn
@@ -34,7 +35,7 @@ print(device)
 ################################################################
 
 # TRAIN DATASET #
-whole_train_dataset = tools.shd_to_dataset('./data/trainX_4ms.npy', './data/trainY_4ms.npy')
+whole_train_dataset = tools.shd_to_dataset('data/trainX_4ms.npy', 'data/trainY_4ms.npy')
 
 # 8156 sequences in whole training dataset
 total_train_dataset_size = len(whole_train_dataset)
@@ -52,7 +53,7 @@ train_dataset, val_dataset = random_split(
 )
 
 # TEST DATASET #
-test_dataset = tools.shd_to_dataset('./data/testX_4ms.npy', './data/testY_4ms.npy')
+test_dataset = tools.shd_to_dataset('data/testX_4ms.npy', 'data/testY_4ms.npy')
 
 # 2264 sequences in test dataset
 test_dataset_size = len(test_dataset)
@@ -64,6 +65,7 @@ test_dataset_size = len(test_dataset)
 sequence_length = 250
 input_size = 700
 hidden_size = 128
+
 num_classes = 20
 batch_size = 32
 
@@ -102,55 +104,67 @@ test_loader = DataLoader(
 ################################################################
 # Model helpers and model setup
 ################################################################
-rand_num = random.randint(1, 10000)
+
+delta = 0.01 #4e-3
 
 # recorded into comment
 # fraction of the elements in the hidden.linear.weight to be zero
 mask_prob = 0.0
 
-# ALIF alpha tau_mem init normal dist.
-adaptive_tau_mem_mean = 20.
-adaptive_tau_mem_std = 5.
+# omega init uniform distribution CHECK on rf.py!
+omega_a = 5.
+omega_b = 10.
 
-# ALIF rho tau_adp init normal dist.
-adaptive_tau_adp_mean = 150.
-adaptive_tau_adp_std = 10.
+# b_offset init uniform distribution CHECK on rf.py!
+b_offset_a = 2.
+b_offset_b = 3.
 
-# LI alpha tau_mem init normal distribution
+# LI tau_mem init normal distribution
 out_adaptive_tau_mem_mean = 20.
 out_adaptive_tau_mem_std = 5.
 
 label_last = False
-sub_seq_length = 10
+sub_seq_length = 0
 
-hidden_bias = True
-output_bias = True
+hidden_bias = False
+output_bias = False
 
-model = snn.models.SimpleALIFRNN(
+model = snn.models.SimpleVanillaRFRNN(
     input_size=input_size,
     hidden_size=hidden_size,
     output_size=num_classes,
-    mask_prob=mask_prob,
-    adaptive_tau_mem_mean=adaptive_tau_mem_mean,
-    adaptive_tau_mem_std=adaptive_tau_mem_std,
-    adaptive_tau_adp_mean=adaptive_tau_adp_mean,
-    adaptive_tau_adp_std=adaptive_tau_adp_std,
+    adaptive_omega_a=omega_a,
+    adaptive_omega_b=omega_b,
+    adaptive_b_offset_a=b_offset_a,
+    adaptive_b_offset_b=b_offset_b,
     out_adaptive_tau_mem_mean=out_adaptive_tau_mem_mean,
     out_adaptive_tau_mem_std=out_adaptive_tau_mem_std,
     label_last=label_last,
     sub_seq_length=sub_seq_length,
-    hidden_bias=hidden_bias,
-    output_bias=output_bias
+    mask_prob=mask_prob,
+    output_bias=False,
+    dt=delta
 ).to(device)
+
+# TORCH SCRIPT #
+model = torch.jit.script(model)
+
+# num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+# print(num_params)
+# 108820
 
 ################################################################
 # Setup experiment (optimizer etc.)
 ################################################################
 
-criterion = torch.nn.NLLLoss()
+# prevent overwriting in slurm
+rand_num = random.randint(1, 10000)
+
+criterion = torch.nn.NLLLoss()  # CrossEntropyLoss()
 
 optimizer_lr = 0.075
 gradient_clip_value = 1.
+
 
 optimizer = torch.optim.Adam(model.parameters(), lr=optimizer_lr)
 
@@ -160,23 +174,21 @@ epochs_num = 20
 
 # learning rate scheduling
 scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: 1 - epoch / epochs_num)
-learning_rates = []
 
 # [logging] Only thing manually changed in the string: Optimizer, criterion and scheduler!
-opt_str = "{}_Adam({}),NLL,LinearLR,ll({}),no_gc".format(rand_num, optimizer_lr, label_last)
-net_str = "RSNN(700,128,20,sub_seq({}),bs_{},ep_{}_bias)"\
-    .format(sub_seq_length, batch_size, epochs_num)
-unit_str = "ALIF(tau_m({},{}),tau_a({},{}),linMask_{})LI(tau_m({},{}))"\
-    .format(adaptive_tau_mem_mean, adaptive_tau_mem_std, adaptive_tau_adp_mean, adaptive_tau_adp_std, mask_prob,
-            out_adaptive_tau_mem_mean, out_adaptive_tau_mem_std)
+opt_str = "{}_Adam({}),NLL,script-fgiDG,LinLR,LL({},no_gc)".format(rand_num, optimizer_lr, label_last)
+net_str = "700,{},20,bs_{},ep_{}"\
+    .format(hidden_size, batch_size, epochs_num)
+unit_str = "RF(omega{},{}b{},{})LI({},{})"\
+    .format(omega_a, omega_b, b_offset_a, b_offset_b, out_adaptive_tau_mem_mean, out_adaptive_tau_mem_std)
 
 comment = opt_str + "," + net_str + "," + unit_str
 
 writer = SummaryWriter(comment=comment)
-log_dir = writer.log_dir
-subprocess.Popen(["tensorboard", "--logdir", log_dir, "--port", "6010"])
+log_dir = writer.log_dir  # This gets the actual directory where logs are being saved
+subprocess.Popen(["tensorboard", "--logdir", log_dir, "--port", "6011"])
 start_time = datetime.now().strftime("%m-%d_%H-%M-%S")
-print(start_time, comment)
+
 
 save_path = "models/{}_".format(start_time) + comment + ".pt"
 save_init_path = "models/{}_init_".format(start_time) + comment + ".pt"
@@ -184,8 +196,9 @@ save_init_path = "models/{}_init_".format(start_time) + comment + ".pt"
 # save initial parameters for analysis
 torch.save({'model_state_dict': model.state_dict()}, save_init_path)
 
-print(model.state_dict())
-print_every = 115
+# print(model.state_dict())
+print_every = 230
+print(comment)
 
 ################################################################
 # Training loop
@@ -214,8 +227,6 @@ for epoch in range(epochs_num + 1):
 
         # Perform validation
         for i, (inputs, targets) in enumerate(val_loader):
-
-            current_batch_size = len(inputs)
 
             # Reshape inputs in [sequence_length, batch_size, data_size].
             input = inputs.permute(1, 0, 2).to(device)
@@ -256,7 +267,7 @@ for epoch in range(epochs_num + 1):
         )
 
         # Persist current best model.
-        if val_loss <= min_val_loss:
+        if val_loss < min_val_loss:
             min_val_loss = val_loss
             min_val_epoch = epoch
             best_model_state_dict = model.state_dict()
@@ -270,11 +281,10 @@ for epoch in range(epochs_num + 1):
 
         test_loss = 0
         test_correct = 0
+        test_total_spikes = 0
 
         # Perform Inference
         for i, (inputs, targets) in enumerate(test_loader):
-
-            current_batch_size = len(inputs)
 
             # Reshape inputs in [sequence_length, batch_size, data_size].
             input = inputs.permute(1, 0, 2).to(device)
@@ -282,7 +292,10 @@ for epoch in range(epochs_num + 1):
             # Reshape targets (for MNIST it's a single pattern).
             target = targets.to(device=device)
 
-            outputs, _, _ = model(input)
+            outputs, _, num_spikes1 = model(input)
+
+            # accumulate total spikes
+            test_total_spikes += num_spikes1.item()
 
             # Apply loss sequentially against single pattern.
             loss = tools.apply_seq_loss(criterion=criterion, outputs=outputs, target=target)
@@ -299,11 +312,9 @@ for epoch in range(epochs_num + 1):
             batch_correct = tools.count_correct_predictions(outputs.mean(dim=0), target)
             test_correct += batch_correct
 
-        # Average test loss
         test_loss /= len(test_loader)  # test_dataset_size
-
-        # Average test accuracy
         test_accuracy = (test_correct / test_dataset_size) * 100.0
+        test_sop = test_total_spikes / test_dataset_size
 
         # Log current test loss and accuracy
         writer.add_scalar(
@@ -319,22 +330,20 @@ for epoch in range(epochs_num + 1):
 
         print(
             "Epoch [{:4d}/{:4d}]  |  Summary  |  Loss/val: {:.6f}, Accuracy/val: {:.4f}%  |  Loss/test: {:.6f}, "
-            "Accuracy/test: {:.4f}".format(
-                epoch, epochs_num, val_loss, val_accuracy, test_loss, test_accuracy), flush=True
+            "Accuracy/test: {:.4f} | SOP: {}".format(
+                epoch, epochs_num, val_loss, val_accuracy, test_loss, test_accuracy, test_sop), flush=True
         )
 
     # Update logging outputs
     writer.flush()
 
     # TRAIN #
-
-    # Run until 299 epochs, the last loop only for the test/valid.
+    # Run until 19 epochs, the last loop only for the test/valid.
     if epoch < epochs_num:
 
         # Go into train mode.
         model.train()
 
-        train_correct = 0
         print_train_loss = 0
         print_correct = 0
         print_total = 0
@@ -347,7 +356,7 @@ for epoch in range(epochs_num + 1):
             # Reshape inputs in [sequence_length, batch_size, data_size].
             input = inputs.permute(1, 0, 2).to(device)
 
-            # Reshape targets.
+            # Reshape targets (for MNIST it's a single pattern).
             target = targets.to(device=device)
 
             # Clear previous gradients
@@ -416,7 +425,7 @@ for epoch in range(epochs_num + 1):
         break
 
 writer.close()
-
 print(tools.PerformanceCounter.time(run_time), "seconds")
 print("Minimum val loss: {:.6f} at epoch: {}".format(min_val_loss, min_val_epoch))
 # print(best_model_state_dict)
+
